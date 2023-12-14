5f5ea202b056013da21eb70b4143e50de23c1fd7
sahomelabs <sahomelabs@gmail.com>
2023-12-14 10:28:42 -0500

    update server function

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 // server.js 
+const express = require('express');
+const app = express();
+
+app.get ('/', function (req,res) {
+    res.send("Hello World");
+});
+
 app.listen(3000, function () {
     console.log('Server is running on port 3000');
-})
+});
 // app port 3000 for listen to Http Request
